3d2111c2cca86ad7ff902c853683a207658b0e98
Fareed Patel <fareed_patel@hotmail.co.uk>
2016-03-25 15:34:20 +0000

    Revert "11 delete my recipes"

--- frontend/e2e-tests/scenarios.js
+++ frontend/e2e-tests/scenarios.js
@@ -1,19 +1,25 @@
 'use strict';
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
+
 describe('recipesApp', function() {
+
+
   it('should automatically redirect to /recipes-view when location hash/fragment is empty', function() {
     browser.get('index.html');
     expect(browser.getLocationAbsUrl()).toMatch("/index");
   });
+
+
   describe('/recipes', function() {
+
     it('should be able to add recipes', function(){
       browser.get('index.html');
       element(by.id("New_recipe")).click();
       element(by.id('name')).sendKeys("chicken something");
       element(by.id("submit")).click();
       element(by.id("See_recipe")).click();
-      expect(element.all(by.css('[ng-view] a')).last().getText()).toMatch(/chicken something/);
+      expect(element.all(by.css('[ng-view] a')).last().getText()).toMatch(/chicken something/)
     })
     it('should have instructions', function() {
       browser.get('index.html');
@@ -24,20 +30,10 @@ describe('recipesApp', function() {
       element(by.id("See_recipe")).click();
       browser.sleep(3000);
       element(by.id("chickennewonetotrythedetails")).click();
+
       expect(element.all(by.css('[ng-view]')).getText()).toMatch(/cook for 30 minutes/);
     })
-    it('should be able to delete recipes', function(){
-      browser.get('index.html');
-      element(by.id("New_recipe")).click();
-      element(by.id('name')).sendKeys("chickentikkamasala");
-      element(by.id('instructions')).sendKeys('cook quickly');
-      element(by.id('submit')).click();
-      element(by.id("See_recipe")).click();
-      element(by.id("chickentikkamasala")).click();
-      element(by.id("Delete_recipe")).click();
-      element(by.id("See_recipe")).click();
-      expect(element(by.id("chickentikkamasala")).isPresent()).toBe(false);
-    });
+
     it('should have instructions', function() {
       browser.get('index.html');
       element(by.id("New_recipe")).click();
@@ -53,4 +49,5 @@ describe('recipesApp', function() {
       expect(element.all(by.css('[ng-view]')).getText()).toMatch(/Open packet and enjoy/);
     })
   })
+
 });
